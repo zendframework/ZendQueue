95a5bb1a3ba231803919efdd02d26a6536499dc3
ingo <ingo.walz@googlemail.com>
2012-10-10 01:41:56 +0200

    Fixed unit tests (most likely namespace issues)
    Change-Id: Ib82455b8203dee57ca1e450d5d9e3405b304ae29

--- tests/ZendQueue/QueueTest.php
+++ tests/ZendQueue/QueueTest.php
@@ -10,7 +10,7 @@
 
 namespace ZendQueueTest;
 
-use Zend\Queue;
+use ZendQueue\Queue;
 use Zend\Log;
 use Zend\Log\Writer;
 use ZendQueue\Adapter;
@@ -39,7 +39,7 @@ class QueueTest extends \PHPUnit_Framework_TestCase
             'params'    => array(),
         );
 
-        $this->queue = new Queue\Queue('ArrayAdapter', $this->config);
+        $this->queue = new Queue('ArrayAdapter', $this->config);
     }
 
     protected function tearDown()
@@ -48,9 +48,9 @@ class QueueTest extends \PHPUnit_Framework_TestCase
 
     public function testConst()
     {
-        $this->assertTrue(is_string(Queue\Queue::TIMEOUT));
-        $this->assertTrue(is_integer(Queue\Queue::VISIBILITY_TIMEOUT));
-        $this->assertTrue(is_string(Queue\Queue::NAME));
+        $this->assertTrue(is_string(Queue::TIMEOUT));
+        $this->assertTrue(is_integer(Queue::VISIBILITY_TIMEOUT));
+        $this->assertTrue(is_string(Queue::NAME));
     }
 
     /**
@@ -70,11 +70,11 @@ class QueueTest extends \PHPUnit_Framework_TestCase
 
         $zend_config = new \Zend\Config\Config($config);
 
-        $obj = new Queue\Queue($config);
-        $this->assertTrue($obj instanceof Queue\Queue);
+        $obj = new Queue($config);
+        $this->assertTrue($obj instanceof Queue);
 
-        $obj = new Queue\Queue($zend_config);
-        $this->assertTrue($obj instanceof Queue\Queue);
+        $obj = new Queue($zend_config);
+        $this->assertTrue($obj instanceof Queue);
     }
 
     public function test_getConfig()
@@ -87,21 +87,21 @@ class QueueTest extends \PHPUnit_Framework_TestCase
     public function test_set_getAdapter()
     {
         $adapter = new Adapter\ArrayAdapter($this->config);
-        $this->assertTrue($this->queue->setAdapter($adapter) instanceof Queue\Queue);
+        $this->assertTrue($this->queue->setAdapter($adapter) instanceof Queue);
         $this->assertTrue($this->queue->getAdapter($adapter) instanceof Adapter\ArrayAdapter);
     }
 
     public function test_set_getMessageClass()
     {
         $class = 'test';
-        $this->assertTrue($this->queue->setMessageClass($class) instanceof Queue\Queue);
+        $this->assertTrue($this->queue->setMessageClass($class) instanceof Queue);
         $this->assertEquals($class, $this->queue->getMessageClass());
     }
 
     public function test_set_getMessageSetClass()
     {
         $class = 'test';
-        $this->assertTrue($this->queue->setMessageSetClass($class) instanceof Queue\Queue);
+        $this->assertTrue($this->queue->setMessageSetClass($class) instanceof Queue);
         $this->assertEquals($class, $this->queue->getMessageSetClass());
     }
 
@@ -131,7 +131,7 @@ class QueueTest extends \PHPUnit_Framework_TestCase
         // isExists
         $queue = 'test';
         $new = $this->queue->createQueue($queue);
-        $this->assertTrue($new instanceof Queue\Queue);
+        $this->assertTrue($new instanceof Queue);
         $this->assertFalse($this->queue->createQueue($queue));
 
         $this->assertTrue($new->deleteQueue());
